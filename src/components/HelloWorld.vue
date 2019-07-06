2b47e04ea4c99a8042abfbb63af693a893f5ae43
baze <wrrwrr111@gmail.com>
2019-07-07 00:03:44 +0800

    add
    新增棋子 棋盘 选手功能 能一个player下棋

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="hello">
-    <div v-for="item,key in board.valueOf()"> {{item}}</div>
+    <div v-if="board" style="height:400px">
+      <table style="z-index:0;position:absolute">
+        <tr v-for="(row,y) in board.boardMatrix.valueOf()" :key="y" class="board row">
+          <td v-for="(col,x) in row" :key="x" class="board col" :class="col?'red':''">
+            {{col}}
+          </td>
+        </tr>
+      </table>
+      <table style="z-index:1;position:absolute" v-if="selectedPiece">
+        <tr v-for="(row,y) in board.previewMatrix.valueOf()" :key="y" class="board row">
+          <td v-for="(col,x) in row" :key="x" class="board col" :class="col?'red':''" @mouseover="test(x,y,selectedPiece)" @click="move()">
+            {{col}}
+          </td>
+        </tr>
+      </table>
+    </div>
     <hr>
-    <div v-for="player,key in players">
-      <p>player{{key+1}}</p>
+    <button @click="rotate">旋转选中</button>
+    <button @click="mirror">翻转选中</button>
+    <div v-for="(player,index) in players" :key="index">
+      <p>player{{index}}</p>
+      <div v-if="selectedPiece">
+        <table>
+          <tr v-for="(row,index) in selectedPiece.matrixData.valueOf()" :key="index" class="piece row">
+            <td v-for="(col,index) in row" class="piece col" :class="col?'red':''">
+              {{col}}
+            </td>
+          </tr>
+        </table>
+      </div>
       <div style="display:flex;flex-wrap:wrap">
-        <div v-for="piece,key in player.pieces">
-          <div v-for="item,key in piece.pieceMatrix.valueOf()"> {{item}}</div>
+        <div v-for="(piece,index) in player.pieces" :key="index" style="margin:20px" @click="selectPiece(piece,index)">
+          <table>
+            <tr v-for="(row,index) in piece.matrixData.valueOf()" :key="index" class="piece row">
+              <td v-for="(col,index) in row" class="piece col" :class="col?'red':''">
+                {{col}}
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
       <hr>
@@ -16,7 +48,6 @@
 <script>
 import math from 'mathjs/dist/math.js'
 
-
 import blokus from '@/libs/blokus'
 
 export default {
@@ -24,23 +55,48 @@ export default {
   data() {
     return {
       // board: math.zeros(20, 20)
-      board: math.zeros(20, 20),
-      players: []
+      board: new blokus.Board(),
+      players: [],
+      selectedPiece: null, //当前选中棋子
     }
   },
   mounted() {
-    for (let id = 1; id <= 4; id++) {
-      this.players.push(new blokus.Player(id))
-    }
-    console.log(this.player)
-
+    this.players.push(new blokus.Player(1,this.board))
   },
   methods: {
-
+    test(row, col,selectedPiece) {
+      this.players[0].tryMove(row, col,selectedPiece)
+    },
+    move(){
+      this.players[0].move()
+      this.selectedPiece = null;
+    },
+    selectPiece(piece,index) {
+      this.selectedPiece = piece
+      this.selectedIndex = index
+      this.board.initTry()
+      console.log(this.selectedPiece)
+    },
+    rotate() {
+      this.selectedPiece && this.selectedPiece.rotate()
+    },
+    mirror() {
+      this.selectedPiece && this.selectedPiece.mirror()
+    }
   }
 }
 
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.col {
+  width: 16px;
+  height: 16px;
+  line-height: 16px;
+}
+
+.red {
+  background-color: red;
+}
+
 </style>
